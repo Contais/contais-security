6e9ef19f98ee1bac9198a998231240fa19c4eadb
Contais <523800546@qq.com>
2018-08-10 01:18:03 +0800

    maven架构初步搭建完毕

---
+++ contais-security-demo/src/main/java/com/contais/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.contais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Contais
+ * @version 1.0
+ * @description com.contais
+ * @date 2018/8/10
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+}
