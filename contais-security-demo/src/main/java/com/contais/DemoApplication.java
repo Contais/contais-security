45d21c15fb54f54ae22950e37a557a2bb3688a61
LinHan <linhan@zhihuijianshe.com>
2018-08-10 09:44:56 +0800

    Hello Spring Security

--- contais-security-demo/src/main/java/com/contais/DemoApplication.java
+++ contais-security-demo/src/main/java/com/contais/DemoApplication.java
@@ -2,6 +2,8 @@ package com.contais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Contais
@@ -10,9 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2018/8/10
  */
 @SpringBootApplication
+@RestController
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring Security";
     }
 }
